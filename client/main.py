7a607b1736b527ccd3ffd4831ecc52a307bf931c
krispo58 <156099152+krispo58@users.noreply.github.com>
2025-12-14 19:14:03 +0100

    nah

--- client/main.py
+++ client/main.py
@@ -1,6 +1,8 @@
+import time
 import client as networkclient
 import wordwrapper
 import pythoncom
+import pywintypes
 
 
 server_ip = "51.175.238.64"
@@ -9,23 +11,48 @@ domain = "ordbokene.no"
 
 word = wordwrapper.WordWrapper(visible=True)
 client = networkclient.Client(server_ip, server_port, domain)
+word_is_open = True
 
+
+def reset(args: list[str]):
+    client.new_chat()
+
+def test(args: list[str]):
+    word.write_end("t")
+    word.replace_text("::test::", "")
+
+commands = {
+    "reset": reset,
+    "test": test
+}
 def find_prompt_replace(word: wordwrapper.WordWrapper):
     word.make_hidden_visible()
     prompt = word.get_block("-", "-")
     if prompt is None:
         return
     r = client.send_prompt(prompt)
-    print(r)
-    word.replace_block(",,", ",,",r)
-    word.replace_block("-", "-", "") #Delete prompt from document
+    word.replace_block(",,", ",,", r)
+    word.replace_text(f"-{prompt}-", "") #Delete prompt from document
 
 def handle_deactivated(word: wordwrapper.WordWrapper):
-    find_prompt_replace(word)
-    print("Flashing taskbar")
-    word.flash_taskbar(1)
+    global word_is_open
+
+    prompt = word.get_block("--", "--") is not None
+    command = word.get_block("::", "::")
+
+    try:
+        if prompt:
+                find_prompt_replace(word)
+                print("Flashing taskbar")
+                word.flash_taskbar(1)
+        if command is not None:
+            commands[command]([word.get_block(",,", ",,")])
+    except pywintypes.com_error as e:
+        print("Word disconnected, waiting for reconnect...")
+        word_is_open = False
 
 def main():
+    global word_is_open
     pythoncom.CoInitialize()
     
     try:
@@ -48,7 +75,18 @@ def main():
     word.on_word_deactivated = handle_deactivated
 
     while True:
-        pythoncom.PumpWaitingMessages()
+        while word_is_open:
+            pythoncom.PumpWaitingMessages()
+
+        #If execution reaches here, word got disconnected
+        while True:
+            time.sleep(1)
+            if word.try_reconnect():
+                print("Word reconnected")
+                word.write_start(res_text)
+                word_is_open = True
+                break
+            print("Failed to reconnect")
 
 if __name__ == "__main__":
     main()
